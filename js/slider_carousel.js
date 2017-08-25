025586123b506e504a25e2f265029df84f33e178
Damian Osiecki <mlodybarney@gmail.com>
2017-08-25 12:24:44 +0200

    teortycznie dziala

--- js/slider_carousel.js
+++ js/slider_carousel.js
@@ -1,19 +1,11 @@
    $(document).ready(function() {
-            $("#content-slider").lightSlider({
-                loop: true,
-                keyPress: true,
-                controls:false,
-                speed: 500,
-                auto: true,
-                loop: true
-            });
             $('#image-gallery').lightSlider({
-                controls:false,
-                gallery: true,
                 item: 4,
                 slideMargin: 0,
+                pager: false,
                 speed: 500,
                 auto: true,
+                controls:true,
                 loop: true,
                 onSliderLoad: function() {
                     $('#image-gallery').removeClass('cS-hidden');
